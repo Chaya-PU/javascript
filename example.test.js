875a9f863b05ffe48af0dee6fbc9317cb414262b
Chaya-PU <chaya.pu@diggibyte.com>
2022-03-10 11:53:12 +0530

    example.test.js

--- example.test.js
+++ example.test.js
@@ -1,7 +1,22 @@
 /* const ternaryOperator = require('./callbackFunction');
 test("to check 2 number" ,()=>{expect(ternaryOperator(5,10)).toBe(false)}); */
+
 /* const toCheckGivenDay = require('./branching_stmt')
 test("to check given day" ,()=>{expect(toCheckGivenDay(3)).toBe("Tuesday")}); */
-const displayAll = require('./set')
-test("to display entries from set" ,()=>{expect(displayAll()).toBe("Tuesday")});
+
+/* const callbackFunction = require('./callback')
+test("print statement after 1 second",()=>{expect(callback()).toBe("Hello World")});
+
+ */const createObject = require('./Object');
+test("display an object",()=>{
+    var person={
+        name:"john",
+        birthYear:1999,
+       
+        age:function(){
+            return 2022-this.birth-year
+        }
+    }
+    expect(JSON.stringify(createObject())).toBe(JSON.stringify(person))
+});
 
